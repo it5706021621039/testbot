684b89ac4dc8d28ced0fa2e4780d29d1e147d5e6
it5706021621039 <5706021621039@fitm.kmutnb.ac.th>
2017-06-14 16:14:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const client = new line.Client({
-  channelAccessToken: '1520004907'
-});
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var cors = require('cors')
 
-const message = {
-  type: 'text',
-  text: 'Hello World!'
-};
+app.use(cors())
+app.use(bodyParser.json())
 
-client.replyMessage('kCz7tuLk8Ox8ptZvYvLn0tB5dIZKkfQwFceq5SVNVlst6gkBC5R6N8pnxEcyp4oZASv1VeAMXO9d7zgx7FHV53qViU7G/4V1lRdTbFvg7aDxHNTFLDZYKBXO2STB6FhrYU07LyObdn3rQ14qbxe1kAdB04t89/1O/w1cDnyilFU=', message)
-  .then(() => {
+app.set('port', (process.env.PORT || 4000))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-  })
-  .catch((err) => {
-    // error handling
-  });
+
+app.get('/', function (req, res) {
+	res.send('Hello')
+})
+
+app.listen(app.get('port'), function () {
+  console.log('run at port', app.get('port'))
+})
